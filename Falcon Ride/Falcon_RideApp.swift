455028aa037d9dbf881359f69ff53933145fe209
Abdulmalik Ariyo <malikariyo@hotmail.com>
2023-11-13 10:21:41 -0700

    Initial Commit

---
+++ Falcon Ride/Falcon_RideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Falcon_RideApp.swift
+//  Falcon Ride
+//
+//  Created by Abdulmalik Ariyo on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Falcon_RideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
